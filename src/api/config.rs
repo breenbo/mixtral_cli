6d67252b42ca2cf439d5fb4bab6ca7e76c4914e0
Bruno Berrehuel <bruno@homepad.ch>
2024-02-25 21:53:02 +0100

    Get/set config file with api_key

--- src/api/config.rs
+++ src/api/config.rs
@@ -6,8 +6,8 @@ use super::AppError;
 
 #[derive(Default, Debug, Deserialize, Serialize)]
 pub struct ConfigFile {
-    api_key: String,
-    pub api_key_initialized: bool,
+    pub api_key: String,
+    api_key_initialized: bool,
 }
 
 impl ConfigFile {
@@ -18,23 +18,35 @@ impl ConfigFile {
         }
     }
 
-    pub fn load_config() -> Result<ConfigFile, AppError> {
+    fn load_config() -> Result<ConfigFile, AppError> {
         let config: ConfigFile = confy::load("mixtral_cli", "config")?;
         Ok(config)
     }
 
-    pub fn set_api_key() -> Result<(), AppError> {
-        println!("Enter your MixtralAi api key: ");
+    fn set_api_key() -> Result<ConfigFile, AppError> {
+        println!("\nEnter your MixtralAi api key: ");
         io::stdout().flush()?;
         let mut key = String::new();
         io::stdin().read_line(&mut key)?;
         let api_key = String::from(key.trim_end());
 
-        let config = ConfigFile::new(api_key);
+        let config = Self::new(api_key);
         println!("{:#?}", config);
 
-        confy::store("mixtral_cli", "config", config)?;
+        confy::store("mixtral_cli", "config", &config)?;
 
-        Ok(())
+        Ok(config)
+    }
+
+    pub fn check_config(reset_key: bool) -> Result<String, AppError> {
+        let mut config = Self::load_config()?;
+        // 1. cli: get the reset from cli
+        // 3. api: check if the api key is good by sending a request and get the response
+        // 2. if not initialized or reset asked by user
+        if !config.api_key_initialized || reset_key {
+            config = Self::set_api_key()?;
+        }
+
+        Ok(config.api_key)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,14 @@
-use dotenv::dotenv;
-use std::env;
-
 mod api;
 use api::{AppError, ConfigFile, MixtralAiApi};
 
 mod cli;
 
 fn main() -> Result<(), AppError> {
+    //
     // set the api key
-    dotenv()?;
-    let api_key = match env::var("MIXTRAL_API_KEY") {
-        Ok(val) => val,
-        Err(_) => panic!("API key not set"),
-    };
     //
-    // ConfigFile::set_api_key(false)?;
-    //
-    let config = ConfigFile::load_config()?;
-    // 1. cli: get the reset from cli
-    let reset_api = cli::get_reset_api();
-    // 3. api: check if the api key is good by sending a request and get the response
-    // 2. if not initialized or reset asked by user
-    if !config.api_key_initialized || reset_api {
-        dbg!(config);
-        ConfigFile::set_api_key()?;
-    }
-
+    let reset_key = cli::get_reset_api();
+    let api_key = ConfigFile::check_config(reset_key)?;
     //
     //
     // define url endpoint
@@ -38,7 +21,6 @@ fn main() -> Result<(), AppError> {
     // prompt the user in a loop
     //
     loop {
-        //
         //
         let question = cli::get_question(&ai_model)?.trim().to_lowercase();
         //
